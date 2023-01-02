1ee24cf04ef3b74786f922377566314bd186dc25
90sRehem <you@example.com>
2023-01-02 19:21:40 -0300

    chore: add eslint

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,7 +1,4 @@
-module.exports = {
-  extends: ["next", "turbo", "prettier"],
-  rules: {
-    "@next/next/no-html-link-for-pages": "off",
-    "react/jsx-key": "off",
-  },
-};
+// module.exports = {
+//   extends: ["./base.js"],
+// };
+module.exports = require("./base.js");
